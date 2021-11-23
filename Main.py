bd56fe538cac3eb63cd9c6cac003ac1b622e8ced
gvenugo3 <gvenugo3@asu.edu>
2021-11-23 16:30:39 -0700

    Bug Fixes
    Bug fixes

--- Main.py
+++ Main.py
@@ -1,18 +1,34 @@
 import requests
 from requests.auth import HTTPBasicAuth
 import json
-import os
 
-data = {}
-
+# Function to retrieve list of all tickets
 def getData():
-    loginDetails = getDetails()
-    user = loginDetails[0]
-    password = loginDetails[1]
-    url = loginDetails[2]
-    res = requests.get(url + "tickets", auth = HTTPBasicAuth(user,password))
-    return res
+	loginDetails = getDetails()
+	user = loginDetails[0]
+	password = loginDetails[1]
+	url = loginDetails[2]
+	res = requests.get(url + "tickets", auth = HTTPBasicAuth(user,password))
 
+	if(res.status_code >= 400):
+		return false
+
+	return res.json()
+
+# Function to retrieve details of ticket with Ticket ID 'ticketId'
+def getTicket(ticketId):
+	loginDetails = getDetails()
+	user = loginDetails[0]
+	password = loginDetails[1]
+	url = loginDetails[2]
+	res = requests.get(url + "tickets/" + str(ticketId) + ".json", auth = HTTPBasicAuth(user,password))
+	
+	if(res.status_code == 400):
+		return false
+
+	return res.json()
+
+# Reads login.json for user auth details
 def getDetails():
     file = open('login.json')
     data = json.load(file)
